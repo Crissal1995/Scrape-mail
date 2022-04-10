8a7d726aeb262aa72a83ba60d203c46e67aad1e6
Cristiano Salerno <cristianosalerno1@gmail.com>
2022-04-10 15:38:17 +0200

    Reformatted code for black v22
    This seems ugly, but ok, who am I to complain?

--- tests/test_core.py
+++ tests/test_core.py
@@ -11,8 +11,8 @@ def test_core_utility_decimal():
     assert b2h_decimal(base - 1) == "999 B"
     assert b2h_decimal(base) == "1.0 KB"
     assert b2h_decimal(base + 1) == "1.001 KB"
-    assert b2h_decimal(base ** 2) == "1.0 MB"
-    assert b2h_decimal(base ** 2, integer=True) == "1 MB"
+    assert b2h_decimal(base**2) == "1.0 MB"
+    assert b2h_decimal(base**2, integer=True) == "1 MB"
 
 
 def test_core_utility_binary():
@@ -21,5 +21,5 @@ def test_core_utility_binary():
     assert b2h_binary(base) == "1.0 KiB"
     assert b2h_binary(base + 1) == "1.001 KiB"
     assert b2h_binary(base * 2) == "2.0 KiB"
-    assert b2h_binary(base ** 4) == "1.0 TiB"
-    assert b2h_binary(base ** 4, integer=True) == "1 TiB"
+    assert b2h_binary(base**4) == "1.0 TiB"
+    assert b2h_binary(base**4, integer=True) == "1 TiB"
